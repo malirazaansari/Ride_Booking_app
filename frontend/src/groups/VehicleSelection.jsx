3f1351e4989c1b858b9eb9207a157b5055c3a9c1
Ali Raza <137720587+malirazaansari@users.noreply.github.com>
2025-03-14 13:03:35 +0500

    the one error coming on wait and return button solved

--- frontend/src/groups/VehicleSelection.jsx
+++ frontend/src/groups/VehicleSelection.jsx
@@ -164,14 +164,15 @@ const VehicleSelection = ({ onWaitAndReturnConfirmed, isWaitAndReturnDisabled })
             <h2 className="font-bold text-lg">Wait and Return</h2>
             <p className="mt-2 text-sm">
               This option will select your pickup place as the drop-off place too, and will visit the first selected drop-off place.
-              <br /><br />
-              Instructions:
-              <ul className="text-left list-disc list-inside">
-                <li>Your pickup address will be set as the drop-off address.</li>
-                <li>The first selected drop-off address will be added as a via address.</li>
-                <li>The map and address fields will be updated accordingly.</li>
-              </ul>
             </p>
+
+            <p className="mt-2 text-sm">Instructions:</p>
+
+            <ul className="text-left list-disc list-inside">
+              <li>Your pickup address will be set as the drop-off address.</li>
+              <li>The first selected drop-off address will be added as a via address.</li>
+              <li>The map and address fields will be updated accordingly.</li>
+            </ul>
             <div className="flex justify-center gap-4 mt-4">
               <button
                 className="bg-red-500 hover:bg-red-600 px-6 py-2 rounded text-white"
